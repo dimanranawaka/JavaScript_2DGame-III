ace1134855e8f380bf41fa41e72c500f8606f8e3
dimanranawaka <dimanranawaka@gmail.com>
2023-09-03 00:11:59 +0530

    Implement - foreverRunnerGame.js

--- foreverRunnerGame.js
+++ foreverRunnerGame.js
@@ -1,10 +1,23 @@
 class ForeverRunnerGame{
-    constructor() {
+    constructor(id,frameRate,groundOffset,playerOptions,difficulty) {
+        this.canvas = document.getElementById(id);
+        this.ctx = this.canvas.getContext("2d");
+        this.frameRate = frameRate;
+        this.groundY = this.canvas.height - groundOffset;
+        this.difficulty = difficulty;
+        this.initialize();
     }
+
+    initialize() {
+
+    }
+
     update(){
 
     }
     draw(){
 
     }
+
+
 }
