437125242617bf21f317ed2b8760d1b672079160
dimanranawaka <dimanranawaka@gmail.com>
2023-09-03 00:48:54 +0530

    Implement - movement.js

--- movement.js
+++ movement.js
@@ -1,13 +1,48 @@
 class Movement{
-    constructor() {
+    constructor(position,groundY,height,jumpPower,jumpHeight,gravity) {
+        this.position = position;
+        this.groundY = groundY;
+        this.height = height;
+        this.jumpPower = jumpPower;
+        this.jumpHeight = jumpHeight;
+        this.gravity = gravity;
     }
+    // This method will update player movement
     update(){
 
+        // If the player jumping move the player object up
+
+        if(this.isJumping){
+            this.position.add(0,-this.jumpPower);
+        }
+
+        // If the player has reached the maximum jump height set isJumping attributes to false
+
+        if(this.position.y <= this.groundY - this.jumpHeight){
+            this.isJumping = false;
+        }
+
+        // This will make sure player will move to down
+
+        if (!this.isJumping && !this.isGrounded()){
+            this.position.add(0,this.gravity);
+        }
+
+        // This will make sure player will stay the ground level
+
+        if (this.position.y >= this.groundY - this.height){
+            this.position.y = this.groundY - this.height;
+        }
     }
     jump(){
-
+        if(this.isGrounded()){
+            this.isJumping = true;
+        }
     }
-    isGrounded(){
 
+    // This will make sure to returns the player isGrounded true
+
+    isGrounded(){
+        return this.position.y == this.groundY - this.height;
     }
 }
