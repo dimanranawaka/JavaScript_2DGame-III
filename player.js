b0328bbcc5255c1c1a4deda8eaea0b4d01477a5e
dimanranawaka <dimanranawaka@gmail.com>
2023-09-03 01:24:28 +0530

    Implement - player.js

--- player.js
+++ player.js
@@ -1,13 +1,44 @@
 class Player{
-    constructor() {
+    constructor(position, animator, movement, collider ) {
+        this.position = position;
+        this.animator = animator;
+        this.movement = movement;
+        this.collider = collider;
     }
+
+    // this method is  used to update player movements and animations
     update(){
-
+        this.movement.update();
+        if (this.movement.isGrounded()){
+            this.animator.update();
+        }
     }
-    draw(){
 
+    //this method will draw player
+    draw(ctx){
+        ctx.beginPath();
+        ctx.drawImage(
+            this.movement.getImage(),
+            this.position.x,
+            this.position.y,
+            this.collider.w,
+            this.collider.h
+        );
+        ctx.closePath();
     }
+    // this will start jumping behaviour
     jump(){
-
+        if (this.movement.isGrounded()){
+            this.movement.jump();
+            this.animator.reset();
+        }
     }
+
+    // This will return true if the collider overlaps with one in the list of colliders
+
+    overlapWithOthers(others){
+        return this.collider.overlapWithOthers(others);
+    }
+
+
 }
