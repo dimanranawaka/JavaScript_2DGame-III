7afcf1f166d074efc6147787add071b22b0438ed
dimanranawaka <dimanranawaka@gmail.com>
2023-09-02 23:51:07 +0530

    Implement - point2d.js

--- point2d.js
+++ point2d.js
@@ -1,3 +1,12 @@
 class Point2d{
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    // method that can increase or decrease x,y values
+    add(dx,dy){
+        this.x+=dx;
+        this.y+=dy;
+    }
 }
