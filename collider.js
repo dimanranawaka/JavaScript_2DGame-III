23200305acd33d5d29056715032c49abbbefca78
dimanranawaka <dimanranawaka@gmail.com>
2023-09-03 01:08:37 +0530

    Implement - collider.js

--- collider.js
+++ collider.js
@@ -1,3 +1,28 @@
 class Collider{
+    constructor(position, w, h) {
+        this.position = position; // This attribute represents the location of colliding object
+        this.h = h; // height of the collider
+        this.w = w; // width of the collider
+    }
+
+    /** this method  can be used to check if the collider overlaps with another collider */
+
+    overlaps(other){
+        return this.position.x < other.position.x + other.w
+        && this.position.x + this.w > other.position.x
+        && this.position.y < other.position.y + other.h
+        && this.position.y + this.h > other.position.y;
+    }
+
+    /** this method can be used returns true if the collider overlaps with one in the list of colliders */
+
+    overlapsWithOthers(others) {
+        for (let i = 0; i < others.length; i++) {
+            if (this.overlaps(others[i])) {
+                return true;
+            };
+        return false;
+        }
+    }
 
 }
