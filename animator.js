41a8dce98328c9ea0aa22543cda6c70079dbe148
dimanranawaka <dimanranawaka@gmail.com>
2023-09-02 23:18:26 +0530

    Implement - animator.js

--- animator.js
+++ animator.js
@@ -1,10 +1,14 @@
 class Animator{
-    constructor() {
+    constructor(playSpeed, showTime , images) {
         this.timer = 0;
         this.index = 0;
+        this.images = images;
+        this.playspeed = playSpeed;
+        this.showTime = showTime;
     }
+    // A method used to update the animation over time.
     update(){
-
+        this.timer +=this.playspeed;
     }
     getImage(){
 
